2b889f5c97ba914a9aad7e25f38efaba3b26cc87
Dmitri <56200546+Syritx@users.noreply.github.com>
2021-01-21 09:36:18 -0500

    increased the delay

--- src/obj-c app/ViewController.m
+++ src/obj-c app/ViewController.m
@@ -32,7 +32,7 @@
     NSData *data = [[NSData alloc] initWithData:[@"[get_data]" dataUsingEncoding:NSASCIIStringEncoding]];
     [outputStream write:[data bytes] maxLength:[data length]];
     
-    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC));
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1 * NSEC_PER_SEC));
     dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
     
         uint8_t buffer[1024];
